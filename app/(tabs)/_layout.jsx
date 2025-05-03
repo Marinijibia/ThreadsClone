3457473527c0a7aa36cbcb50e2c28feda164fec4
codeBaron <80063210+Marinijibia@users.noreply.github.com>
2025-05-03 01:06:33 +0100

    Pushing the project setup and first tab navigation setup

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,46 @@
+import { Feather } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+
+
+const HomeTabsLayout = () => {
+    return (
+        <Tabs screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: "#000",
+            tabBarInactiveTintColor: "#888",
+            tabBarShowLabel: false,
+        }}>
+            <Tabs.Screen 
+                name="index" 
+                options={{ 
+                    title: "Feed",
+                    tabBarIcon: ({ size, color }) => <Feather name="home" size={size} color={color} />
+                }}
+            />
+            <Tabs.Screen 
+                name="search" 
+                options={{ 
+                    title: "Search",
+                    tabBarIcon: ({ size, color }) => <Feather name="search" size={size} color={color} /> 
+                }} 
+            />
+            <Tabs.Screen 
+                name="notifications" 
+                options={{ 
+                    title: "Notifications",
+                    tabBarIcon: ({ size, color }) => <Feather name="heart" size={size} color={color} /> 
+                }} 
+            />
+            <Tabs.Screen 
+                name="profile" 
+                options={{ 
+                    title: "Profile",
+                    tabBarIcon: ({ size, color }) => <Feather name="user" size={size} color={color} />
+                }} 
+            />
+        </Tabs>
+    )
+}
+
+export default HomeTabsLayout; 
